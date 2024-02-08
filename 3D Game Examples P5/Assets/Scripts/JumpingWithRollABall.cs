ed1ff0f08c6bb177301f1a550ca3658214577fed
Justin Scott <hello@jrs.dev>
2024-02-08 14:22:23 -0800

    Finishing example

--- 3D Game Examples P5/Assets/Scripts/JumpingWithRollABall.cs
+++ 3D Game Examples P5/Assets/Scripts/JumpingWithRollABall.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class JumpingWithRollABall : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public int score = 0;
     public float Speed = 10f;
     public float JumpForce = 10f;
     public float GravityModifier = 1f;
@@ -9,7 +12,9 @@ public class JumpingWithRollABall : MonoBehaviour
     public bool IsOnGround = true;
     private float _horizontalInput;
     private float _forwardInput;
+    private bool _isAtCheckpoint = false;
     private Vector3 _startingPosition;
+    private Vector3 _checkpointPosition;
     private Rigidbody _playerRigidbody;
 
     // Start is called before the first frame update
@@ -18,6 +23,7 @@ public class JumpingWithRollABall : MonoBehaviour
         _playerRigidbody = GetComponent<Rigidbody>();
         Physics.gravity *= GravityModifier;
         _startingPosition = transform.position;
+        scoreText.text = "Score: " + score.ToString();
     }
 
     // Update is called once per frame
@@ -34,7 +40,14 @@ public class JumpingWithRollABall : MonoBehaviour
 
         if(transform.position.y < OutOfBounds)
         {
-            transform.position = _startingPosition;
+            if(_isAtCheckpoint)
+            {
+                transform.position = _checkpointPosition;
+            }
+            else
+            {
+                transform.position = _startingPosition;
+            }
         }
     }
 
@@ -51,13 +64,38 @@ public class JumpingWithRollABall : MonoBehaviour
         {
             IsOnGround = true;
         }
+
+        if(collision.gameObject.CompareTag("Dead Zone"))
+        {
+            if(_isAtCheckpoint)
+            {
+                transform.position = _checkpointPosition;
+            }
+            else
+            {
+                transform.position = _startingPosition;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Checkpoint"))
         {
-            _startingPosition = other.gameObject.transform.position;
+            _isAtCheckpoint = true;
+            _checkpointPosition = other.gameObject.transform.position;
+        }
+        if(other.gameObject.CompareTag("Endpoint"))
+        {
+            _isAtCheckpoint = false;
+            transform.position = _startingPosition;
+        }
+
+        if(other.gameObject.CompareTag("Collectible"))
+        {
+            score++;
+            scoreText.text = "Score: " + score.ToString();
+            Destroy(other.gameObject);
         }
     }
 }
---
+++ 3D Game Examples P5/Assets/Scripts/Spinners.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Spinners : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
